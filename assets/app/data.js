0a5ae31152f5211b429f4bbfa8e3a91d69645364
PROMIS BASUNIA <126019991+developer-promis@users.noreply.github.com>
2023-07-08 18:33:31 +0600

    Add files via upload

---
+++ assets/app/data.js
@@ -0,0 +1,105 @@
+const post_list = [
+  {
+    id: 1,
+    post_publish_date: `5h`,
+    post_content: `
+    
+    #EidUlAdhaMubarak
+    `,
+      post_attachment: `./assets/images/post_image/rakib.jpg`,
+    likes: "83",
+    comments: "17",
+    author: {
+      name: "Rakibb ",
+      author_url: `https://web.facebook.com/rahatul.rakib.5`,
+      author_image:`./assets/images/author/rakib.jpg`,
+    },
+  },
+  {
+    id: 2,
+    post_publish_date: `21h`,
+    post_content: `
+   ✌️✌️
+    `,
+    post_attachment: `./assets/images/post_image/meraj.jpg`,
+    likes: "45",
+    comments: "25",
+    author: {
+      name: "Mon Marajul",
+      author_url: `https://web.facebook.com/mdmarajul.islam.1806`,
+      author_image:`./assets/images/author/meraj.jpg`,
+
+    },
+  },
+  {
+    id: 3,
+    post_publish_date: `12h`,
+    post_content: `
+    🥰🥰
+    `,
+    post_attachment: `./assets/images/post_image/wahid.jpg`,
+
+    likes: "100",
+    comments: "50",
+    author: {
+      name: "Limon",
+      author_url: `https://web.facebook.com/profile.php?id=100085706851382`,
+      author_image:`./assets/images/author/wahid.jpg`,
+
+    },
+  },
+  {
+    id: 4,
+    post_publish_date: `10h`,
+    post_content: `
+    🙂
+    `,
+    post_attachment: `./assets/images/post_image/naim.jpg`,
+
+    likes: "80",
+    comments: "45",
+    author: {
+      name: "Deowan Mohammad Naim",
+      author_url: `https://web.facebook.com/profile.php?id=100067990127179`,
+      author_image:`./assets/images/author/naim.jpg`,
+
+    },
+  },
+
+  
+  {
+    id: 7,
+    post_publish_date: `7h`,
+    post_content: `
+    
+    →Eid Mubarak Everyone 💙    
+    `,
+    post_attachment: `./assets/images/post_image/sahin.jpg`,
+
+    likes: "220",
+    comments: "56",
+    author: {
+      name: `Shahin Bin Salim`,
+      author_url: `https://web.facebook.com/profile.php?id=100047389165360`,
+      author_image:`./assets/images/author/sahin.jpg`,
+
+    },
+  },
+  {
+    id: 8,
+    post_publish_date: `4m`,
+    post_content: `বৃষ্টির সময়, বারান্দার সামনে...
+
+    `,
+    post_attachment: `./assets/images/post_image/junu.jpg`,
+
+    likes: "26",
+    comments: "4",
+    author: {
+      name: `Junaid Aman Junu`,
+      author_url: `https://web.facebook.com/junaid.amc`,
+      author_image:`./assets/images/author/junu.jpg`,
+
+    },
+  },
+];
